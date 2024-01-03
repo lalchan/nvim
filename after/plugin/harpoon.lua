1117c6359d8a03fbad05cb8d460d67a30d314c8b
Adarsha Lalchan <scorpiclalchan@gmail.com>
2024-01-03 18:17:36 +0545

    optimized some harpoon shortcuts

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -5,8 +5,14 @@ harpoon:setup({})
 vim.keymap.set("n", "<leader>ha", function() harpoon:list():append() end, {
 	desc = "Mark File"
 })
-vim.keymap.set("n", "<leader>hh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, {
+vim.keymap.set("n", "<leader>hf", function() harpoon.ui:toggle_quick_menu(harpoon:list(), { border = "none" }) end, {
 	desc = "Open Menu"
+})
+vim.keymap.set("n", "<leader>hh", function() harpoon:list():prev() end, {
+	desc = "Previous Buffer"
+})
+vim.keymap.set("n", "<leader>hl", function() harpoon:list():next() end, {
+	desc = "Next Buffer"
 })
 
 vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
