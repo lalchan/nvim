afc24de84f55bd4775335598e669f426b3323150
Adarsha Lalchan <scorpiclalchan@gmail.com>
2024-01-09 15:06:02 +0545

    added shortcuts for terminal from vim

--- lua/lalchan/keymaps.lua
+++ lua/lalchan/keymaps.lua
@@ -7,7 +7,9 @@ vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = tr
 
 vim.keymap.set('n', '<leader>c', vim.cmd.noh, { desc = "Clear Highlights" })
 
-
 vim.keymap.set('n', '<leader>dk', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
 vim.keymap.set('n', '<leader>dj', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>dd', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+
+vim.keymap.set('n', '<leader>t', vim.cmd.terminal, { desc = 'Terminal' })
+vim.keymap.set('t', '<C-t>', "<C-\\><C-n>", { desc = 'Exit Terminal' })
