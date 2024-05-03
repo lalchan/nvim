8f1a7bae0a1080cfac72a866af2c2454a3c05d40
lalchan <scorpiclalchan@gmail.com>
2024-05-03 16:14:11 +0545

    added prettier config

--- lua/plugin/lazy/conform.lua
+++ lua/plugin/lazy/conform.lua
@@ -1,10 +1,9 @@
--- Autoformat
 return {
 	"stevearc/conform.nvim",
-	lazy = false,
+	event = "LspAttach",
 	keys = {
 		{
-			"=",
+			"<leader>cf",
 			function()
 				require("conform").format({ async = true, lsp_fallback = true })
 			end,
@@ -13,25 +12,74 @@ return {
 		},
 	},
 	opts = {
-		notify_on_error = false,
+		quiet = true,
+		formatters_by_ft = {
+			typescript = { { "prettierd", "prettier" } },
+			typescriptreact = { { "prettierd", "prettier" } },
+			javascript = { { "prettierd", "prettier" } },
+			javascriptreact = { { "prettierd", "prettier" } },
+			json = { { "prettierd", "prettier" } },
+			html = { { "prettierd", "prettier" } },
+			css = { { "prettierd", "prettier" } },
+			scss = { { "prettierd", "prettier" } },
+			markdown = { { "prettierd", "prettier" } },
+			yaml = { { "prettierd", "prettier" } },
+			lua = { "stylua" },
+			sh = { "beautysh" },
+			zsh = { "beautysh" },
+		},
 		format_on_save = function(bufnr)
-			-- Disable "format_on_save lsp_fallback" for languages that don't
-			-- have a well standardized coding style. You can add additional
-			-- languages here or re-enable it for the disabled ones.
+			-- Disable autoformat for files in a certain path
+			local bufname = vim.api.nvim_buf_get_name(bufnr)
+			if bufname:match("/node_modules/") then
+				return
+			end
 			local disable_filetypes = { c = true, cpp = true, env = true }
 			return {
 				timeout_ms = 500,
 				lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+				async = true,
 			}
 		end,
-		formatters_by_ft = {
-			lua = { "stylua" },
-			-- Conform can also run multiple formatters sequentially
-			-- python = { "isort", "black" },
-			--
-			-- You can use a sub-list to tell conform to run *until* a formatter
-			-- is found.
-			javascript = { { "prettierd", "prettier" } },
-		},
+		format_after_save = { lsp_fallback = true },
 	},
+	config = function(_, opts)
+		local conform = require("conform")
+		conform.setup(opts)
+
+		-- Customize prettier args
+		---@diagnostic disable-next-line: unused-local
+		require("conform.formatters.prettier").args = function(self, ctx)
+			local prettier_roots = { ".prettierrc", ".prettierrc.json", "prettier.config.js" }
+			local args = { "--stdin-filepath", "$FILENAME" }
+
+			local localPrettierConfig = vim.fs.find(prettier_roots, {
+				upward = true,
+				path = ctx.dirname,
+				type = "file",
+			})[1]
+
+			if localPrettierConfig then
+				vim.list_extend(args, { "--config", localPrettierConfig })
+			end
+
+			local hasTailwindPrettierPlugin = vim.fs.find("node_modules/prettier-plugin-tailwindcss", {
+				upward = true,
+				path = ctx.dirname,
+				type = "directory",
+			})[1]
+
+			if hasTailwindPrettierPlugin then
+				vim.list_extend(args, { "--plugin", "prettier-plugin-tailwindcss" })
+			end
+
+			return args
+		end
+
+		conform.formatters.beautysh = {
+			prepend_args = function()
+				return { "--indent-size", "2", "--force-function-style", "fnpar" }
+			end,
+		}
+	end,
 }
