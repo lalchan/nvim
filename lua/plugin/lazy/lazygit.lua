1826bb422e3fc2042c3a3421fd2410c11d08fc4d
lalchan <scorpiclalchan@gmail.com>
2024-06-03 12:45:49 +0545

    fix: moved all git related shortcuts to leader g prefix

--- lua/plugin/lazy/lazygit.lua
+++ lua/plugin/lazy/lazygit.lua
@@ -6,6 +6,6 @@ return {
 	-- setting the keybinding for LazyGit with 'keys' is recommended in
 	-- order to load the plugin when the command is run for the first time
 	keys = {
-		{ "<leader>eg", "<cmd>LazyGit<cr>", desc = "[E]xplore [G]it" },
+		{ "<leader>gg", "<cmd>LazyGit<cr>", desc = "[E]xplore [G]it" },
 	},
 }
