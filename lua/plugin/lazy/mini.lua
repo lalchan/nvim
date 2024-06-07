3adc09bf7bcce33a6df8e2e39b298a2314fbddfb
lalchan <scorpiclalchan@gmail.com>
2024-06-07 15:49:38 +0545

    feat: enables mipattern for mighlighting patterns

--- lua/plugin/lazy/mini.lua
+++ lua/plugin/lazy/mini.lua
@@ -21,6 +21,18 @@ return {
 
 		require("mini.pairs").setup({})
 
+		-- NOTE
+		require("mini.hipatterns").setup({
+			highlighters = {
+				fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
+				hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
+				todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
+				note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
+				-- Highlight hex color strings (`#rrggbb`) using that color
+				hex_color = require("mini.hipatterns").gen_highlighter.hex_color(),
+			},
+		})
+
 		local animate = require("mini.animate")
 		animate.setup({
 			cursor = {
