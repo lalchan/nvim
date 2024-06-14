493ee712b75953591f249d0faf39acb0450fb14a
lalchan <scorpiclalchan@gmail.com>
2024-06-14 17:35:03 +0545

    fix: removed numhl and and added signcolumn for git status

--- lua/plugin/lazy/gitsign.lua
+++ lua/plugin/lazy/gitsign.lua
@@ -3,9 +3,7 @@
 return {
 	"lewis6991/gitsigns.nvim",
 	opts = {
-		signcolumn = false,
-		numhl = true,
-		-- linehl = true,
+		signcolumn = true,
 		current_line_blame = true,
 		on_attach = function(bufnr)
 			local gitsigns = require("gitsigns")
