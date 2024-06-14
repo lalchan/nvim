7f147ea4b7be2989e46ac21989e539dddab0ff22
lalchan <scorpiclalchan@gmail.com>
2024-06-14 17:13:26 +0545

    feat: removed spearator from treesitter-context

--- lua/plugin/lazy/treesitter-context.lua
+++ lua/plugin/lazy/treesitter-context.lua
@@ -6,7 +6,6 @@ return {
 		return {
 			max_lines = 2, -- How many lines the window should span. Values <= 0 mean no limit.
 			multiline_threshold = 1, -- Maximum number of lines to collapse for a single context line
-			separator = "-",
 		}
 	end,
 }
