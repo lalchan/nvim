5bf285f10bbea11f9cf1ff072b164b00e4d4b958
lalchan <scorpiclalchan@gmail.com>
2024-06-14 15:54:33 +0545

    feat: removed terminal shortcut and changed save buffer shortcut to use leader

--- lua/lalchan/keymaps.lua
+++ lua/lalchan/keymaps.lua
@@ -13,10 +13,6 @@ vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagn
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
---
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
--- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
 -- TIP: Disable arrow keys in normal mode
 vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
@@ -27,7 +23,7 @@ vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
 -- Accessibility Shortcuts
 vim.keymap.set("n", "<leader>ee", ":Oil --float<CR>", { desc = "[E]xplore File [E]xplorer" })
 vim.keymap.set("n", "<leader>qq", ":qa<CR>", { desc = "Quit Vim(no save)" })
-vim.keymap.set("n", "ss", ":w<CR>", { desc = "Save Current Buffer" })
+vim.keymap.set("n", "<leader>ss", ":w<CR>", { desc = "Save Current Buffer" })
 
 -- Open terminal
 vim.keymap.set("n", "<leader>et", ":terminal<CR>", { desc = "[E]xplore [T]erminal" })
