8a69a89e6b6d2b68ac071ee0613fb85077867b8a
lalchan <scorpiclalchan@gmail.com>
2024-06-17 12:14:22 +0545

    lint: naming convention established

--- lua/plugin/lazy/init.lua
+++ lua/plugin/lazy/init.lua
@@ -1,8 +1,8 @@
 local themes = require("plugin/lazy/themes") -- Themes
-local gitsign = require("plugin/lazy/gitsign") -- Adds git related signs to the gutter
-local whichKey = require("plugin/lazy/whichKey") -- Useful plugin to show you pending keybinds
+local gitSign = require("plugin/lazy/git-sign") -- Adds git related signs to the gutter
+local whichKey = require("plugin/lazy/which-key") -- Useful plugin to show you pending keybinds
 local telescope = require("plugin/lazy/telescope") -- Fuzzy Finder
-local treesitter = require("plugin/lazy/treesitter") -- Highlight, edit, and navigate code
+local treeSitter = require("plugin/lazy/tree-sitter") -- Highlight, edit, and navigate code
 local session = require("plugin/lazy/session") -- Session
 local dashboard = require("plugin/lazy/dashboard") -- Dashboard
 local oil = require("plugin/lazy/oil") -- File explorer
@@ -10,19 +10,19 @@ local harpoon = require("plugin/lazy/harpoon") -- Harpoon
 local neogit = require("plugin/lazy/neogit")
 local flash = require("plugin/lazy/flash")
 local codeium = require("plugin/lazy/codeium") -- Codeium
-local undotree = require("plugin/lazy/undotree") -- Undotree
+local undoTree = require("plugin/lazy/undo-tree") -- Undotree
 local mini = require("plugin/lazy/mini") -- Collection of various small independent plugins/modules
-local treesittercontext = require("plugin/lazy/treesitter-context")
+local treeSitterContext = require("plugin/lazy/tree-sitter-context")
 local cmp = require("plugin/lazy/cmp") -- Auto Complete
 local lsp = require("plugin/lazy/lsp") -- LSP
 local conform = require("plugin.lazy.conform") -- Formatter
 
 return {
 	themes,
-	gitsign,
+	gitSign,
 	whichKey,
 	telescope,
-	treesitter,
+	treeSitter,
 	session,
 	dashboard,
 	oil,
@@ -30,9 +30,9 @@ return {
 	neogit,
 	flash,
 	codeium,
-	undotree,
+	undoTree,
 	mini,
-	treesittercontext,
+	treeSitterContext,
 	lsp,
 	cmp,
 	conform,
