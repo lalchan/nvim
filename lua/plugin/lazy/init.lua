cfcf937a455712bd409be7f1919c65e6c24c7565
lalchan <scorpiclalchan@gmail.com>
2024-06-17 12:20:29 +0545

    fix: changed plugin import style

--- lua/plugin/lazy/init.lua
+++ lua/plugin/lazy/init.lua
@@ -1,20 +1,20 @@
-local themes = require("plugin/lazy/themes") -- Themes
-local gitSign = require("plugin/lazy/git-sign") -- Adds git related signs to the gutter
-local whichKey = require("plugin/lazy/which-key") -- Useful plugin to show you pending keybinds
-local telescope = require("plugin/lazy/telescope") -- Fuzzy Finder
-local treeSitter = require("plugin/lazy/tree-sitter") -- Highlight, edit, and navigate code
-local session = require("plugin/lazy/session") -- Session
-local dashboard = require("plugin/lazy/dashboard") -- Dashboard
-local oil = require("plugin/lazy/oil") -- File explorer
-local harpoon = require("plugin/lazy/harpoon") -- Harpoon
-local neogit = require("plugin/lazy/neogit")
-local flash = require("plugin/lazy/flash")
-local codeium = require("plugin/lazy/codeium") -- Codeium
-local undoTree = require("plugin/lazy/undo-tree") -- Undotree
-local mini = require("plugin/lazy/mini") -- Collection of various small independent plugins/modules
-local treeSitterContext = require("plugin/lazy/tree-sitter-context")
-local cmp = require("plugin/lazy/cmp") -- Auto Complete
-local lsp = require("plugin/lazy/lsp") -- LSP
+local themes = require("plugin.lazy.themes") -- Themes
+local gitSign = require("plugin.lazy.git-sign") -- Adds git related signs to the gutter
+local whichKey = require("plugin.lazy.which-key") -- Useful plugin to show you pending keybinds
+local telescope = require("plugin.lazy.telescope") -- Fuzzy Finder
+local treeSitter = require("plugin.lazy.tree-sitter") -- Highlight, edit, and navigate code
+local session = require("plugin.lazy.session") -- Session
+local dashboard = require("plugin.lazy.dashboard") -- Dashboard
+local oil = require("plugin.lazy.oil") -- File explorer
+local harpoon = require("plugin.lazy.harpoon") -- Harpoon
+local neogit = require("plugin.lazy.neogit")
+local flash = require("plugin.lazy.flash")
+local codeium = require("plugin.lazy.codeium") -- Codeium
+local undoTree = require("plugin.lazy.undo-tree") -- Undotree
+local mini = require("plugin.lazy.mini") -- Collection of various small independent plugins.modules
+local treeSitterContext = require("plugin.lazy.tree-sitter-context")
+local cmp = require("plugin.lazy.cmp") -- Auto Complete
+local lsp = require("plugin.lazy.lsp") -- LSP
 local conform = require("plugin.lazy.conform") -- Formatter
 
 return {
