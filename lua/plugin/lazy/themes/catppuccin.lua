56ca5866c62fa59d96ee477b9034affd4ad9f396
lalchan <scorpiclalchan@gmail.com>
2024-06-20 17:45:24 +0545

    feat: removed transparency from theme

--- lua/plugin/lazy/themes/catppuccin.lua
+++ lua/plugin/lazy/themes/catppuccin.lua
@@ -4,12 +4,5 @@ return {
 	priority = 1000,
 	config = function()
 		vim.cmd.colorscheme("catppuccin")
-		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-		vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-
-		vim.api.nvim_set_hl(0, "CursorLine", { bg = "none" })
-		vim.api.nvim_set_hl(0, "LineNr", { fg = "#ffcaca" })
-		vim.api.nvim_set_hl(0, "StatusLine", { fg = "#ffcaca" })
-		vim.api.nvim_set_hl(0, "StatusLineNC", { fg = "#ffcaca" })
 	end,
 }
